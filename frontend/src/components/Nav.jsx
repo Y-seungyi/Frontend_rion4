16a511f4a52bcd24dd0c1c39d0813c7fe58f35d6
eunseo <jangeun4097@naver.com>
2024-07-04 03:32:16 +0900

    ㄴㅇㄴㅇ

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -1,29 +1,41 @@
 import "./Nav.css";
 
 const Nav = () => {
+  const userName = localStorage.getItem("userName");
 
-    const navHome = () => {
-        window.location.replace(`../`);
-    }
+  const navHome = () => {
+    window.location.replace(`../`);
+  };
 
-    const navLogin = () => {
-        window.location.replace(`../login`);
-    }
+  const navLogin = () => {
+    window.location.replace(`../login`);
+  };
 
-    const navWrite = () => {
-        window.location.replace(`../create`);
-    }
+  const navLogout = () => {
+    // localStorage에서 userName 삭제
+    localStorage.removeItem("userName");
+    // 로그아웃 후 홈으로 이동
+    window.location.replace(`../`);
+  };
 
-	return (
-		<>
-			<div className="navBar">
-				<div className="logo">금스타그램</div>
-				<div onClick={navHome}>Home</div>
-				<div onClick={navLogin}>Login</div>
-				<div onClick={navWrite}>Write</div>
-			</div>
-		</>
-	);
+  const navWrite = () => {
+    window.location.replace(`../create`);
+  };
+
+  return (
+    <>
+      <div className="navBar">
+        <div className="logo">금스타그램</div>
+        <div onClick={navHome}>Home</div>
+        {userName ? (
+          <div onClick={navLogout}>Logout</div>
+        ) : (
+          <div onClick={navLogin}>Login</div>
+        )}
+        <div onClick={navWrite}>Write</div>
+      </div>
+    </>
+  );
 };
 
 export default Nav;
